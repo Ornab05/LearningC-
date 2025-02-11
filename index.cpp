d30d280b0a1d240ef0fdf64ef4639e7d3d6c97cf
Ornab Purkayastha <ornabpurkayastha@Ornabs-MacBook-Pro.local>
2025-02-11 16:51:30 -0500

    first commit

---
+++ index.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // string ornab;
+
+    // cin>> ornab;
+
+
+    // cout<< ornab <<" is the big bull of market";
+    // return 0;
+
+// char ch = 'a';
+//     int i = 0;
+//     cout << "A char uses " << sizeof(ch) << " byte(s).\n";
+//     cout << "An int uses " << sizeof(i) << " byte(s).\n";
+//     return 0;
+
+
+
+int count(0), problems(100);
+
+cout<< count << ".\a how it is going    " << problems;
+
+cout<< "kemon acho" << ".\n amra goribs";
+cout<< ".\n" << problems;
+
+return 0;
+
+
+
+
+
+
+}
---
+++ new.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using std::cout;
+
+int main()
+{
+    cout << "Hello world brotherss.\n";
+    cout <<"how is it going";
+    
+    return 0;
+}
