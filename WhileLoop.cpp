7d10e2d49ac80f4c390823f13433dfa4ae2ba81e
Ornab Purkayastha <ornabpurkayastha@mac.mynetworksettings.com>
2025-02-11 22:58:40 -0500

    using while loop

---
+++ WhileLoop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// how many greetings do you want
+
+
+int main() {
+
+int count;
+
+cout<< "How many greetings do you want?.\n";
+cin >> count;
+
+while (count>0){
+    cout << "Hello.\n";
+    count--;
+}
+
+
+
+
+return 0;
+
+
+}
